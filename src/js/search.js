a0ed783db539d0540bab0feb58d5206324eff8b6
Victor <victor3790@gmail.com>
2020-09-06 20:26:37 -0500

    Toggle effect added to search controls

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+
     $('#rate_slider').ship_slider({
         title:'PRECIO SEMANAL (€)', 
         min:2000, 
@@ -16,4 +17,20 @@ jQuery(document).ready(function($){
         min:1, 
         max:21
     });
+
+    $('.search__arrow').click(function(){
+        if($(this).hasClass('search__arrow--shows'))
+        {
+            $(this).removeClass('search__arrow--shows');
+            $(this).addClass('search__arrow--hides');
+            $('.search__controls').toggle('slow');
+        }
+        else if($(this).hasClass('search__arrow--hides'))
+        {
+            $(this).removeClass('search__arrow--hides');
+            $(this).addClass('search__arrow--shows');
+            $('.search__controls').toggle('slow');
+        }
+    });
+
 });
